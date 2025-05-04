0890884dc8ad0da2babdb56ae6509ad2dc6b9c6c
Frigg <glaucia.belo.dias@gmail.com>
2025-05-03 23:18:10 -0300

    inicio de controllers

--- ecommerce-app/routes/web.php
+++ ecommerce-app/routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\CarController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,18 +11,38 @@ Route::get('/', function () {
     // dd($person);
     // dump($person);
 
+    // $aboutPageUrl = '/about';
+
+    // $aboutPageUrl = route('about');
+    $productUrl = route('product.view',['lang'=> 'en', 'id' => 1]);
+    dd($productUrl);
+
     return view('welcome');
 });
 
+Route::get('{lang}/product/{id}', function(string $lang, string $id){
+
+})->name('product.view');
+
+Route::fallback(function() {
+    return "Rota de fallback";
+});
 // [
 // EQUIVALENTES
-Route::view(uri:'/about', view:'about');
+Route::view(uri:'/about', view:'about')->name('about');
 
 // Route::get('/about', function() {
 //     return view('about');
 // });
 
 // ]
+
+Route::controller(CarController::class)->group(function(){
+    Route::get('/car', 'index');
+});
+
+
+
 
 
 
@@ -39,3 +59,4 @@ Route::view(uri:'/about', view:'about');
 // Route::get(uri:'/product/{id}', action: function(string $id){
 //     return "Works! $id";
 // }) -> whereNumber('id');
+
